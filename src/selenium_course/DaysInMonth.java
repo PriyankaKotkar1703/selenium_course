576bdcbd2b12ba62e3bc3fab358bf5483ac3672d
priya <hp@hp>
2023-11-30 16:12:55 +0530

    Days In Month

---
+++ src/selenium_course/DaysInMonth.java
@@ -0,0 +1,68 @@
+package selenium_course;
+import java.util.Scanner;
+
+public class DaysInMonth {
+
+	public static void main(String[] args) {
+		 int month;
+		 Scanner p=new Scanner(System.in);
+
+		    System.out.print("Enter month number (1-12): ");
+		    month=p.nextInt();
+		    if(month == 1)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else if(month == 2)
+		    {
+		        System.out.println("28 or 29 days");
+		    }
+		    else if(month == 3)
+		    {
+		        System.out.println("31 days");
+		    }
+		else if(month == 4)
+		    {
+		        System.out.println("30 days");
+		    }
+		    else if(month == 5)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else if(month == 6)
+		    {
+		        System.out.println("30 days");
+		    }
+		    else if(month == 7)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else if(month == 8)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else if(month == 9)
+		    {
+		       System.out.println("30 days");
+		    }
+		else if(month == 10)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else if(month == 11)
+		    {
+		        System.out.println("30 days");
+		    }
+		    else if(month == 12)
+		    {
+		        System.out.println("31 days");
+		    }
+		    else
+		    {
+		        System.out.println("Invalid input! Please enter month number between (1-12).");
+		    }
+
+
+	}
+
+}
