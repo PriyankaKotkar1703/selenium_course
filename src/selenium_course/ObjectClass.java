c11516ab0e38b5b3c0f90d861676040609a566dd
priya <hp@hp>
2023-12-01 00:15:08 +0530

    Objects of class Example

---
+++ src/selenium_course/ObjectClass.java
@@ -0,0 +1,176 @@
+package selenium_course;
+
+public class ObjectClass {
+
+	public static void main(String[] args) {
+	}
+		class book
+		{
+			double length;
+			double width;
+			int pages;
+			String size;
+			String colour;
+			
+			void Write(String notes)
+			{
+				
+			}
+		}
+		
+		
+		class Chair
+		{
+			double length;
+			double height;
+			String colour;
+			String material;
+			
+			void Seat()
+			{
+				
+			}
+		}
+		
+		class Bike
+		{
+			String material;
+			double avg;
+			int speed;
+			String colour;
+			String shape;
+			
+			void rideBike()
+			{
+				
+			}
+			
+		}
+		
+		
+		class WashingMachine
+		{
+			double length;
+			double height;
+			double width;
+			String colour;
+			int capacity;
+			String shape;
+			double weight;
+			
+			void washCloths()
+			{
+				
+			}
+		
+		}
+		
+		class House
+		{
+			double length;
+			double height;
+			double width;
+			String name;
+			String shape;
+			int houseNo;
+			
+			
+			void live()
+			{
+				
+			}
+		}
+		
+		class Pencil
+		{
+			double weight;
+			double height;
+			double width;
+			String color;
+			
+			void toWrite()
+			{
+				
+			}
+		
+		}
+		
+		class smartphone
+		{
+			double width;
+			double screensize;
+			double speed;
+			String shape;
+			String processorname;
+			String simtype;
+			 
+			void calling()
+			{
+				
+			}
+		}
+		
+		class Fan
+		{
+			int Blades;
+			String shape;
+			String material;
+			double weight;
+			String Brand_Name;
+			void spreadsAir()
+			{
+				
+			}
+		}
+		
+		class Lift
+		{
+			double height;
+			double width;
+			double weightCapacity;
+			String BrandName;
+		void CarryPassenger ()
+			{
+				
+			}
+		}
+		
+		class WaterPurifier
+		{
+			double height;
+			double width;
+			double waterCapacity;
+			String shape;
+			String colour;
+		void filterWater()
+			{
+				
+			}
+			
+		}
+		
+		class Aeroplane
+		{
+			double height;
+			double weight;
+			double width;
+			String colour;
+			String name;
+			int PassengerCapacity;
+			
+			void transport()
+			{
+				
+			}
+		}
+		
+		
+		
+
+	}
+
+
+
+	
+
+
