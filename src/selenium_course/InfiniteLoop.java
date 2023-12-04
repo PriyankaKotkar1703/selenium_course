33254071378e606236f8e91ae9f2c8bb4c9e8ea6
priya <hp@hp>
2023-12-04 18:58:25 +0530

    infinite for loop 1

---
+++ src/selenium_course/InfiniteLoop.java
@@ -0,0 +1,12 @@
+package selenium_course;
+
+public class InfiniteLoop {
+	 
+		  public static void main(String[] args) { 
+		    for(;;) {
+		    System.out.println("Hello"); 
+		    } 
+		  } 
+		}
+			
+
