b33dee3657b408f76f9afef4111f1dc88e634308
priya <hp@hp>
2023-12-07 01:58:05 +0530

    time

---
+++ src/selenium_course/Time.java
@@ -0,0 +1,17 @@
+package selenium_course;
+
+public class Time {
+
+	public static void main(String[] args) {
+ 
+				int secondsin = 86399 ;
+				int seconds = secondsin % 60;
+				int hour = secondsin /60;
+				int minute = hour % 60;
+			    hour = hour /60;
+			    System.out.println(hour+"Hour "+": "+minute+"minute : "+seconds+" seconds");
+
+
+	}
+
+}
