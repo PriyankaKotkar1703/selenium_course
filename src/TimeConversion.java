a2c10604ce7909d0fe17a1b59a68ed4662107366
priya <hp@hp>
2023-12-07 01:43:44 +0530

    time conversion

---
+++ src/TimeConversion.java
@@ -0,0 +1,15 @@
+
+public class TimeConversion {
+
+	public static void main(String[] args) {
+		int secondsin = 86399 ;
+		int seconds = secondsin % 60;
+		int hour = secondsin /60;
+		int minute = hour % 60;
+	    hour = hour /60;
+	    System.out.println(hour+"Hour "+": "+minute+"minute : "+seconds+" seconds");
+		
+
+	}
+
+}
